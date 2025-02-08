83612eb93fd91c1c26a720fe56192e394dc12ca8
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-08 12:38:36 +0100

    fixed some erros

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,7 +1,8 @@
-import { Request, Response } from "express";
-import {User} from "../utils/type";
-import passport from "../passport/passport";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
-dotenv.config();
+import { Request, Response } from "express"
+import sendEmail from "../utils/type"
+import passport from "../passport/passport"
+import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs"
+import dotenv from "dotenv"
+dotenv.config()
 
