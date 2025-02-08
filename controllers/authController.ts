99d5f7e93abe383fe4a1cc1de751dec0fa390974
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-08 12:47:03 +0100

    fixed some erros

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -6,3 +6,8 @@ import bcrypt from "bcryptjs"
 import dotenv from "dotenv"
 dotenv.config()
 
+const JWT_SECRET = process.env.JWT_SECRET as string 
+
+export const signUpWithEmail = async (req: Request, res: Response) => <Promise<any> {
+  
+}
