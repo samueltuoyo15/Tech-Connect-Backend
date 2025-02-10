969f88fb5acb708abf367bfadc712908b0b69e2e
samueltuoyo15 <samueltuoyo9082@gmail.com>
2025-02-10 11:39:56 +0100

    test

--- middlewares/getCurrentUser.ts
+++ middlewares/getCurrentUser.ts
@@ -5,17 +5,18 @@ import User  from "../models/User"
 const JWT_SECRET = process.env.JWT_SECRET as string
 
 
-export const authenticateJWT = async (req: Request, res: Response, next: NextFunction ) => {
+export const authenticateJWT = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
   const token = req.header("Authorization")?.split(" ")[1]
-  if (!token) return res.status(401).json({ message: "Unauthorized" })
+  if (!token) return next(new Error("Unauthorized"))
 
   try {
     const decoded = jwt.verify(token, JWT_SECRET) as { id: string }
     const user = await User.findById(decoded.id).select("-password")
-    if (!user) return res.status(404).json({ message: "User not found" })
+    if (!user) return next(new Error("User not found"))
+
     req.user = user
     next()
   } catch (error) {
-    return res.status(403).json({ message: "Invalid token" })
+    next(new Error("Invalid token"))
   }
 }
--- models/User.ts
+++ models/User.ts
@@ -1,6 +1,7 @@
 import mongoose, {Schema, Document} from "mongoose"
 
 export interface UserInterface extends Document{
+  _id: string;
   fullname: string;
   username: string;
   email: string;
